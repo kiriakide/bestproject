1a2a41e86eae8b5aa2428029d8bf53841504ec49
kiriakide <kiriakigew20@gmail.com>
2022-10-31 11:10:21 +0200

    hi

---
+++ pages/Data.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from PIL import Image
+import pandas as pd
+import base64
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+import requests
+import json
+import time
+
+image = Image.open('logo.jpg')
+
+st.image(image, width = 500)
+
+st.title('Crypto Price App')
+st.markdown("""
+This app retrieves cryptocurrency prices for the top 100 cryptocurrency from the **CoinMarketCap**!
+""")
