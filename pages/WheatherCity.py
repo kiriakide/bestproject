12954275d7584eb53424ba100b0b436870e0dcc8
kiriakide <kiriakigew20@gmail.com>
2022-10-31 13:28:47 +0200

    hi

---
+++ pages/WheatherCity.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import requests
+
+API_KEY = ""
+
+
+def find_current_weather(city):
+    base_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+    weather_data = requests.get(base_url).json()
+    st.json(weather_data)
+
+
+
+def main():
+    st.header("Find the Weather")
+    city = st.text_input("Enter the City").lower()
+    if st.button("Find"):
+        general, temperature, icon = find_current_weather(city)
