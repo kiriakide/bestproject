b0fa93d1c26f3ade9d8dfe2b590cb48888ed8b97
kiriakide <kiriakigew20@gmail.com>
2022-10-31 13:44:01 +0200

    hi

--- pages/Weather.py
+++ pages/Weather.py
@@ -4,19 +4,23 @@ import requests
 API_KEY = "XXXXXXXXXXXXXXXXX"
 
 
+def convert_to_celcius(temperature_in_kelvin):
+    return temperature_in_kelvin - 273.15
+
+
 def find_current_weather(city):
     base_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
     weather_data = requests.get(base_url).json()
+    st.json(weather_data)
     try:
         general = weather_data['weather'][0]['main']
         icon_id = weather_data['weather'][0]['icon']
-        temperature = round(weather_data['main']['temp'])
+        temperature = round(convert_to_celcius(weather_data['main']['temp']))
         icon = f"http://openweathermap.org/img/wn/{icon_id}@2x.png"
     except KeyError:
         st.error("City Not Found")
         st.stop()
     return general, temperature, icon
-
 
 def main():
     st.header("Find the Weather")
@@ -28,6 +32,7 @@ def main():
             st.metric(label="Temperature", value=f"{temperature}°C")
         with col_2:
             st.write(general)
+            st.write(temperature)
             st.image(icon)
 
 
