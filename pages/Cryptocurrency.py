eb1d0d4da30dafe3a733517188a7a5fdc66289e8
kiriakide <kiriakigew20@gmail.com>
2022-11-01 22:04:23 +0200

    hi

--- pages/Cryptocurrency.py
+++ pages/Cryptocurrency.py
@@ -10,10 +10,11 @@ with st.container():
 
 st.sidebar.success("select a page above")
 
-import requests
-
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
 url = 'https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol=ETH&market=USD&interval=5min&apikey=demo'
 response = requests.get(url)
 crypto_data = response.json()
 st.write(crypto_data)
+
+option = st.selectbox ('Select currency for price', ('USD', 'BTC', 'ETH'))
+st.write('You selected',option)
