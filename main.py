2384e914b265ef3315d7d973323a8508c5e97a6a
kiriakide <kiriakigew20@gmail.com>
2022-11-01 22:44:45 +0200

    hi

--- main.py
+++ main.py
@@ -61,10 +61,3 @@ col3.metric(col3_selection, col3_price, col3_percent)
 st.header('**All Price**')
 st.dataframe(df)
 
-st.info('Credit: Created by Chanin Nantasenamat (aka [Data Professor](https://youtube.com/dataprofessor/))')
-
-st.markdown("""
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-""", unsafe_allow_html=True)
--- pages/Cryptocurrency.py
+++ pages/Cryptocurrency.py
@@ -16,10 +16,6 @@ r = requests.get(url)
 data = r.json()
 st.write(data)
 
-option = st.selectbox ('Select currency for price', ('USD', 'BTC', 'ETH'))
-st.write('You selected',option)
-
-
 # Custom function for rounding values
 def round_value(input_value):
     if input_value.values > 1:
@@ -28,16 +24,5 @@ def round_value(input_value):
         a = float(round(input_value, 8))
     return a
 
-col1, col2, col3 = st.columns(3)
-
-# Widget (Cryptocurrency selection box)
-col1_selection = st.sidebar.selectbox('Price 1', url.symbol, list(url.symbol).index('BTCBUSD') )
-col2_selection = st.sidebar.selectbox('Price 2', url.symbol, list(url.symbol).index('ETHBUSD') )
-col3_selection = st.sidebar.selectbox('Price 3', url.symbol, list(url.symbol).index('BNBBUSD') )
-col4_selection = st.sidebar.selectbox('Price 4', url.symbol, list(url.symbol).index('XRPBUSD') )
-col5_selection = st.sidebar.selectbox('Price 5', url.symbol, list(url.symbol).index('ADABUSD') )
-col6_selection = st.sidebar.selectbox('Price 6', url.symbol, list(url.symbol).index('DOGEBUSD') )
-col7_selection = st.sidebar.selectbox('Price 7', url.symbol, list(url.symbol).index('SHIBBUSD') )
-col8_selection = st.sidebar.selectbox('Price 8', url.symbol, list(url.symbol).index('DOTBUSD') )
-col9_selection = st.sidebar.selectbox('Price 9', url.symbol, list(url.symbol).index('MATICBUSD') )
-
+st.header('**All Price**')
+st.dataframe(url)
