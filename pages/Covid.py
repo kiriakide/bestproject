ba55877a42c9dfd93055424e635b4ededf4dc390
kiriakide <kiriakigew20@gmail.com>
2022-11-01 14:40:35 +0200

    hi

--- pages/Covid.py
+++ pages/Covid.py
@@ -18,9 +18,3 @@ response = requests.request("GET", url, headers=headers, params=querystring)
 covid_data = response.json()
 st.write(covid_data)
 
-try:
-
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
