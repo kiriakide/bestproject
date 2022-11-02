a5b2b2da9b7fb452f8e287d1fad4739b84181263
kiriakide <kiriakigew20@gmail.com>
2022-11-02 20:28:35 +0200

    hi

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ st.set_page_config(page_title="Health Coach", page_icon=":heart:", layout="wide"
 with st.container():
     st.title ("Application of 6 widgets :heart:")
     st.caption("hey")
+    st.markdown('Streamlit is **_really_ cool**.')
 
 st.sidebar.success("select a page above")
