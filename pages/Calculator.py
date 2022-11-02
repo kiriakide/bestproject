d761ed2282a8d5eb21c388d075abc7af92e02f19
kiriakide <kiriakigew20@gmail.com>
2022-11-02 21:42:44 +0200

    hi

--- pages/Calculator.py
+++ pages/Calculator.py
@@ -10,15 +10,15 @@ st.title ("Calculator🪙")
 st.markdown("Give two numbers and get the result of the addition")
 st.markdown("This widget was done with AWS Function")
 
-st.subheader("Select an Exchange Crypto Price to Trade")
+st.subheader("Insert 2 Numbers")
 
-
-
+#InsertNumbers
 number1 = st.number_input('Insert a number1')
 st.write('The current number is ', number1)
 number2 = st.number_input('Insert a number2')
 st.write('The current number is ', number2)
 
+#Resultfromfunction
 url1 = "https://gdicyypesekolfiew4kze4i3cy0vylwp.lambda-url.us-east-1.on.aws/?first=%f&second=%f" % (number1, number2)
 info = requests.get(url1)
 st.write(info.text)
