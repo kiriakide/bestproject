5e0cb26bbf97efb3fb2658f749330486c42a7871
kiriakide <kiriakigew20@gmail.com>
2022-11-02 19:42:44 +0200

    hi

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.set_page_config(page_title="Health Coach", page_icon=":heart:", layout="wide" )
 
 with st.container():
-    st.title ("HealthCoach :heart: :running:")
-    st.subheader("hey")
+    st.title ("Application of 6 widgets :heart:")
+    st.caption("hey")
 
 st.sidebar.success("select a page above")
