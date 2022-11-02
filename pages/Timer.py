36dd132f007e433ca614ea31f2a678f5bc82c125
kiriakide <kiriakigew20@gmail.com>
2022-11-02 17:58:25 +0200

    hi

--- pages/Timer.py
+++ pages/Timer.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import time
-import yfinance as yf
 
 def count_down(ts):
     with st.empty():
--- pages/Weather.py
+++ pages/Weather.py
@@ -47,3 +47,13 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+number1 = st.number_input('Insert a number1')
+st.write('The current number is ', number1)
+number2 = st.number_input('Insert a number2')
+st.write('The current number is ', number2)
+
+url1 = "https://gdicyypesekolfiew4kze4i3cy0vylwp.lambda-url.us-east-1.on.aws/?first=%f&second=%f" % (number1, number2)
+info = requests.get(url1)
+st.write(info)
