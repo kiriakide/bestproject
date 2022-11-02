75d80a336ffc01f01923e8d3cf501ac12e8df8b4
kiriakide <kiriakigew20@gmail.com>
2022-11-02 21:21:45 +0200

    hi

--- pages/Cryptocurrency.py
+++ pages/Cryptocurrency.py
@@ -14,6 +14,8 @@ df = pd.read_json('https://api.binance.com/api/v3/ticker/24hr')
 st.subheader('**Market Activity Table**')
 st.dataframe(df)
 
+st.subheader("Select an Exchange Crypto Price to Trade")
+
 col1, col2 = st.columns(2)
 with col1:
     st.checkbox("Disable selection of coin", key="disabled")
@@ -22,7 +24,7 @@ with col2:
     Select_a_coin = st.selectbox('Select a coin', df.symbol, list(df.symbol).index('ETHBTC'),
                                  disabled=st.session_state.disabled,)
 
-st.subheader("Select an Exchange Crypto Price to Trade")
+
 col1_df = df[df.symbol == Select_a_coin]
 
 
