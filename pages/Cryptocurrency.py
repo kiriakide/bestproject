01864168118fbb05c4ed33bcb1e594f1536de2e0
kiriakide <kiriakigew20@gmail.com>
2022-11-02 11:36:35 +0200

    hi

--- pages/Cryptocurrency.py
+++ pages/Cryptocurrency.py
@@ -20,11 +20,12 @@ with col1:
     st.checkbox("Disable selection of coin", key="disabled")
 
 with col2:
-    Select_a_coin = st.selectbox('Select a coin', df.symbol, list(df.symbol).index('BTCBUSD'), disabled=st.session_state.disabled,)
+    Select_a_coin = st.selectbox('Select a coin', df.symbol, list(df.symbol).index('ETHBTC'), disabled=st.session_state.disabled,)
     Select_a_coin2 = st.selectbox('Select a coin', df.symbol, list(df.symbol).index('ETHBUSD'))
 
 
 col1_df = df[df.symbol == Select_a_coin]
+
 
 def round_value(input_value):
     if input_value.values > 1:
@@ -32,6 +33,7 @@ def round_value(input_value):
     else:
         a = float(round(input_value, 8))
     return a
+
 
 col1_price = round_value(col1_df.weightedAvgPrice)
 
