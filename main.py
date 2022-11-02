83c650dc523f9a8fe2d0b92b09333202b3a5b988
kiriakide <kiriakigew20@gmail.com>
2022-11-02 20:39:02 +0200

    hi

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import streamlit as st
-import pandas as pd
 
 st.set_page_config(page_title="Health Coach", page_icon=":heart:", layout="wide" )
 
 with st.container():
     st.title ("Application of 6 widgets :heart:")
-    st.subheader("hey")
+    st.subheader("_Assignment 1_")
     st.caption("hey")
     st.markdown('Streamlit is **_really_ cool**.')
 
