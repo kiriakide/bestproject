d2f73e7f8d1010f06f5fa655459b0b94d1aca619
kiriakide <kiriakigew20@gmail.com>
2022-11-03 21:09:21 +0200

    hi

--- pages/2. Daily Tasks.py
+++ pages/2. Daily Tasks.py
@@ -1,11 +1,23 @@
 import streamlit as st
 import requests
 
+#PageTitle&sidebar
+st.set_page_config(page_title="Calculator", page_icon="🪙", layout="wide" )
+st.sidebar.success("select a page above")
 
+#Heading
+st.title ("Calculator")
+st.caption("This is an interactive widget & was deployed in AWS Function")
+st.markdown("Give two numbers and get the result of the addition")
+
+
+#Function
 url= "https://qnmv7cfexpjgverograjfduuua0dkkvk.lambda-url.us-east-1.on.aws/?planet="
 info = requests.get(url)
 st.write(info.text)
 
+
+#widget
 option = st.selectbox(
     'How would you like to be contacted?',
     ('Email', 'Home phone', 'Mobile phone'))
--- pages/3. Select Correct Answer.py
+++ pages/3. Select Correct Answer.py
@@ -1,14 +1,25 @@
 import streamlit as st
 import requests
 
+#PageTitle&sidebar
+st.set_page_config(page_title="Calculator", page_icon="🪙", layout="wide" )
+st.sidebar.success("select a page above")
 
+#Heading
+st.title ("Select Correct Answer")
+st.caption("This is an interactive widget & was deployed in AWS Function")
+st.markdown("What is Earth?")
+
+
+#Function
 url= "https://qnmv7cfexpjgverograjfduuua0dkkvk.lambda-url.us-east-1.on.aws/?planet="
 info = requests.get(url)
 st.write(info.text)
 
+#Widget
 genre = st.radio(
-    "What's your favorite movie genre",
-    ('Comedy', 'Drama', 'Documentary'))
+    "What is Earth?",
+    ('Book', 'Planet', 'Movie'))
 
 if genre == 'Comedy':
     st.write('You selected comedy.')
