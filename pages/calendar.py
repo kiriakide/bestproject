f541e351414e450b7222b6fee8d723d5883f02df
kiriakide <kiriakigew20@gmail.com>
2022-11-03 14:54:59 +0200

    hi

--- pages/calendar.py
+++ pages/calendar.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import calendar
+import requests
+
 
 d = st.date_input("Enter your date of birth")
 
@@ -8,3 +9,8 @@ st.write('Your birthday is:', d)
 
 val = st.time_input("set timer")
 st.write(val)
+
+
+url= "https://4getmkaykbzgcrbvhgpttlwdaa0yzvyp.lambda-url.us-east-1.on.aws/"
+info = requests.get(url)
+st.write(info.text)
