26dc172195e6b77f1d5f02f25ff70c4bdd3158d0
kiriakide <kiriakigew20@gmail.com>
2022-11-03 15:40:34 +0200

    hi

--- pages/Covid-19.py
+++ pages/Covid-19.py
@@ -10,5 +10,7 @@ col1,col2,col3 = st.columns(3)
 
 with col1:
     st.metric(label="Total Cases", value=(covid_data["regionData"][0-100]["totalCases"]), delta=(covid_data["regionData"][0-100]["newCases"]))
+with col2:
     st.metric(label="Deaths", value=(covid_data["regionData"][0-100]["totalDeaths"]), delta=(covid_data["regionData"][0-100]["newDeaths"]))
+with col3:
     st.metric(label="Recovered", value=(covid_data["regionData"][0 - 100]["totalRecovered"]), delta=(covid_data["regionData"][0 - 100]["newRecovered"]))
