9168397c6f12c1ab3c660d0c56660cc1cb30376c
kiriakide <kiriakigew20@gmail.com>
2022-11-03 10:16:25 +0200

    hi

--- pages/calendar.py
+++ pages/calendar.py
@@ -5,3 +5,13 @@ c = calendar.TextCalendar(calendar.SUNDAY)
 ok = c.formatmonth(2025,1)
 
 st.write(ok)
+
+#adding a date input widget
+
+d = st.date_input("Enter your date of birth")
+
+
+
+#displaying the date entered by the user
+
+st.write('Your birthday is:', d)
