21a84c5d1385ffbcbb1637621c9b910e360fa6d6
kiriakide <kiriakigew20@gmail.com>
2022-11-03 14:32:11 +0200

    hi

--- pages/calendar.py
+++ pages/calendar.py
@@ -1,17 +1,10 @@
 import streamlit as st
 import calendar
 
-c = calendar.TextCalendar(calendar.SUNDAY)
-ok = c.formatmonth(2025,1)
-
-st.write(ok)
-
-#adding a date input widget
-
 d = st.date_input("Enter your date of birth")
 
 
-
-#displaying the date entered by the user
-
 st.write('Your birthday is:', d)
+
+val = st.time_input("set timer")
+st.write(val)
