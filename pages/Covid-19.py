a8a112c9405d5d592a61a9d5dc9f445716cd7c46
kiriakide <kiriakigew20@gmail.com>
2022-11-03 15:15:48 +0200

    hi

--- pages/Covid-19.py
+++ pages/Covid-19.py
@@ -1,14 +1,10 @@
 import streamlit as st
 import requests
 
-
-
-
 base_url = "https://api.apify.com/v2/key-value-stores/SmuuI0oebnTWjRTUh/records/LATEST?disableRedirect=true."
 covid_data = requests.get(base_url).json()
 st.write(covid_data)
 
-st.metric(label="Last Updated:", value= (covid_data["data"]["last_update"]))
 
 col1,col2,col3 = st.columns(3)
 
