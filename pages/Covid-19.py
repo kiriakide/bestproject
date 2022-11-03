599ddcf1d9f39490768f3bdd86e2e926bf377447
kiriakide <kiriakigew20@gmail.com>
2022-11-03 15:10:42 +0200

    hi

--- pages/Covid-19.py
+++ pages/Covid-19.py
@@ -1,8 +1,16 @@
 import streamlit as st
 import requests
+import pandas as pd
 
 
 
 base_url = "https://api.apify.com/v2/key-value-stores/SmuuI0oebnTWjRTUh/records/LATEST?disableRedirect=true."
-weather_data = requests.get(base_url).json()
-st.write(weather_data)
+covid_data = requests.get(base_url).json()
+st.write(covid_data)
+
+
+df = pd.read_json("https://api.apify.com/v2/key-value-stores/SmuuI0oebnTWjRTUh/records/LATEST?disableRedirect=true.")
+
+#MarketTable
+st.header('**Market Acetivity Table**')
+st.dataframe(df)
