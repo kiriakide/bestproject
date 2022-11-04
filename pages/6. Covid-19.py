2ceff1c84642a96b772980522454a0b948523872
kiriakide <kiriakigew20@gmail.com>
2022-11-04 20:46:57 +0200

    hi

--- pages/6. Covid-19.py
+++ pages/6. Covid-19.py
@@ -25,6 +25,31 @@ with col2:
 with col3:
     st.metric(label="Recovered", value=(covid_data["regionData"][0 - 100]["totalRecovered"]), delta=(covid_data["regionData"][0 - 100]["newRecovered"]))
 
-code = '''def hello():
-    print("Hello, Streamlit!")'''
+code = '''import streamlit as st
+import requests
+
+#Headings
+st.set_page_config(page_title="Covid Cases", page_icon=":anger:", layout="wide" )
+
+with st.container():
+    st.title ("Covid Cases 🦠")
+    st.markdown("Get the latest data about covid-19 around te World")
+    st.markdown("This is a widget that retrieves data directly from **Worldometer website**")
+st.sidebar.success("select a page above")
+
+#Apiimport
+base_url = "https://api.apify.com/v2/key-value-stores/SmuuI0oebnTWjRTUh/records/LATEST?disableRedirect=true."
+covid_data = requests.get(base_url).json()
+
+
+#widget
+col1,col2,col3 = st.columns(3)
+
+with col1:
+    st.metric(label="Total Cases", value=(covid_data["regionData"][0-100]["totalCases"]), delta=(covid_data["regionData"][0-100]["newCases"]))
+with col2:
+    st.metric(label="Deaths", value=(covid_data["regionData"][0-100]["totalDeaths"]), delta=(covid_data["regionData"][0-100]["newDeaths"]))
+with col3:
+    st.metric(label="Recovered", value=(covid_data["regionData"][0 - 100]["totalRecovered"]), delta=(covid_data["regionData"][0 - 100]["newRecovered"]))
+'''
 st.code(code, language='python')
