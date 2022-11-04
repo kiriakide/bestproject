41aaaf510447ae54bb3e756fdd88ddbc92d1c0f6
kiriakide <kiriakigew20@gmail.com>
2022-11-04 12:53:15 +0200

    hi

--- pages/3. Select Correct Answer.py
+++ pages/3. Select Correct Answer.py
@@ -11,17 +11,14 @@ st.caption("This is an interactive widget & was deployed in AWS Function")
 st.markdown("Daily Questions to train your brain")
 
 
-#Function
-url= "https://qnmv7cfexpjgverograjfduuua0dkkvk.lambda-url.us-east-1.on.aws/?planet="
-info = requests.get(url)
-st.write(info.text)
-
 #Widget
 genre = st.radio(
     "Question: What is Earth?",
     ('Book', 'Planet', 'Movie'))
 
-if genre == 'Planet':
-    st.write('Correct!')
-else:
-    st.write("Nope try again.")
+
+#Function
+url= "https://qnmv7cfexpjgverograjfduuua0dkkvk.lambda-url.us-east-1.on.aws/?planet=%s" % (genre)
+info = requests.get(url)
+st.write(info.text)
+
