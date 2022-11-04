7e3ddc70b1587457915c41fa0fe95bc99c2e8abd
kiriakide <kiriakigew20@gmail.com>
2022-11-04 20:40:25 +0200

    hi

--- main.py
+++ main.py
@@ -16,4 +16,22 @@ st.markdown("CEI 521 Advanced Topics in Software Engineering ")
 
 st.sidebar.success("select a widget above")
 
+code = '''import streamlit as st
 
+st.set_page_config(page_title="Assignment1", page_icon=":heart:", layout="wide" )
+
+with st.container():
+    st.title ("Application of 6 widgets :heart:")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header("_Assignment 1_")
+with col2:
+    st.header('_Kiriaki Georgiou_')
+
+st.markdown("CEI 521 Advanced Topics in Software Engineering ")
+
+st.sidebar.success("select a widget above")
+'''
+st.code(code, language='python')
