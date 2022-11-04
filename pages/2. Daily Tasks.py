ad7b8ca60da1a0f7f2a26596d5f90a95634f9362
kiriakide <kiriakigew20@gmail.com>
2022-11-04 13:05:58 +0200

    hi

--- pages/2. Daily Tasks.py
+++ pages/2. Daily Tasks.py
@@ -12,21 +12,11 @@ st.markdown("Tick as soon as you do a daily task")
 
 
 #Function
-url= "https://qnmv7cfexpjgverograjfduuua0dkkvk.lambda-url.us-east-1.on.aws/?planet="
+url= "https://df3ajmtjofdu34xvb2mp3y33ce0wtwii.lambda-url.us-east-1.on.aws/?list=" %()
 info = requests.get(url)
 st.write(info.text)
 
 
 #widget
-Task1 = st.checkbox('Groceries')
-Task2 = st.checkbox('Gym')
-Task3 = st.checkbox('Prepare Lunch')
+Task1 = st.checkbox('Groceries','Gym','Read')
 
-if Task1:
-    st.write('Great!')
-
-if Task2:
-    st.write('Great!')
-
-if Task3:
-    st.write('Great!')
