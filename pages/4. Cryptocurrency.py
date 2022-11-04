dc8ee77f67386f51a7cd823062061981724b8737
kiriakide <kiriakigew20@gmail.com>
2022-11-04 20:41:10 +0200

    hi

--- pages/4. Cryptocurrency.py
+++ pages/4. Cryptocurrency.py
@@ -46,6 +46,52 @@ coin_percent = f'{float(coin_selected.priceChangePercent)}%'
 
 col1.metric(Select_a_coin, coin_price, coin_percent)
 
-code = '''def hello():
-    print("Hello, Streamlit!")'''
+code = '''import streamlit as st
+import pandas as pd
+
+#Headings
+st.set_page_config(page_title="Crypto", page_icon="🪙", layout="wide" )
+st.sidebar.success("select a page above")
+
+
+st.title ("Cryptocurrency Prices 🪙")
+st.caption("This is an interactive widget")
+st.markdown("Retrieve the market price of cryptocurrency exchange directly from **Binance Website**")
+
+#API
+df = pd.read_json('https://api.binance.com/api/v3/ticker/24hr')
+
+#MarketTable
+st.header('**Market Acetivity Table**')
+st.dataframe(df)
+
+#WidgetForCrryptoSelection
+st.header("Select an Exchange Crypto Price to Trade")
+
+col1, col2 = st.columns(2)
+with col1:
+    st.checkbox("Disable selection of coin", key="disabled")
+
+with col2:
+    Select_a_coin = st.selectbox('Select a coin', df.symbol, list(df.symbol).index('ETHBTC'),
+                                 disabled=st.session_state.disabled,)
+
+coin_selected = df[df.symbol == Select_a_coin]
+
+#Roundthenumbers
+def round_value(input_value):
+    if input_value.values > 1:
+        a = float(round(input_value, 2))
+    else:
+        a = float(round(input_value, 8))
+    return a
+
+#Showtheselection
+
+coin_price = round_value(coin_selected.weightedAvgPrice)
+
+coin_percent = f'{float(coin_selected.priceChangePercent)}%'
+
+col1.metric(Select_a_coin, coin_price, coin_percent)
+'''
 st.code(code, language='python')
